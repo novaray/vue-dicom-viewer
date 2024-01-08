b868265c7e762832845ed43b0fb7aab951a14403
skkim <novaray@naver.com>
2024-01-08 17:39:25 +0900

    타입 정의 파일 생성.

---
+++ src/types/dicomImageLoader.d.ts
@@ -0,0 +1,7 @@
+declare module '@cornerstonejs/dicom-image-loader' {
+  export const external : any;
+  export const wadouri: any;
+  export const wadors: any;
+  export const webWorkerManager: any;
+  export const configure: any;
+}
