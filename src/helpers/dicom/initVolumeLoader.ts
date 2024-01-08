98e0dbb5fe62a9c735edbf2468a604ab9b8a5a0b
skkim <novaray@naver.com>
2024-01-08 17:39:12 +0900

    cornerstone helper 함수 세팅

---
+++ src/helpers/dicom/initVolumeLoader.ts
@@ -0,0 +1,22 @@
+import { volumeLoader } from '@cornerstonejs/core';
+import {
+  cornerstoneStreamingImageVolumeLoader,
+  cornerstoneStreamingDynamicImageVolumeLoader,
+} from '@cornerstonejs/streaming-image-volume-loader';
+
+export default function initVolumeLoader() {
+  volumeLoader.registerUnknownVolumeLoader(
+    // @ts-ignore
+    cornerstoneStreamingImageVolumeLoader,
+  );
+  volumeLoader.registerVolumeLoader(
+    'cornerstoneStreamingImageVolume',
+    // @ts-ignore
+    cornerstoneStreamingImageVolumeLoader
+  );
+  volumeLoader.registerVolumeLoader(
+    'cornerstoneStreamingDynamicImageVolume',
+    // @ts-ignore
+    cornerstoneStreamingDynamicImageVolumeLoader
+  );
+}
---
+++ src/helpers/dicom/setTitleAndDescription.ts
@@ -0,0 +1,11 @@
+export default function setTitleAndDescription(titleText: string, descriptionText: string) {
+  const title = document.getElementById('demo-title');
+  const description = document.getElementById('demo-description');
+  
+  if (!title || !description) {
+    throw new Error('title or description element not found - setTitleAndDescription.ts');
+  }
+  
+  title.innerText = titleText;
+  description.innerText = descriptionText;
+}
