47ed8e6f9c436f29a5b332e2188bb08e81616631
skkim <novaray@naver.com>
2024-01-09 11:42:04 +0900

    console.log -> console.error

--- src/helpers/dicom/createImageIdsAndCacheMetaData.ts
+++ src/helpers/dicom/createImageIdsAndCacheMetaData.ts
@@ -136,7 +136,7 @@ export default async function createImageIdsAndCacheMetaData({
           );
         });
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     }
   }
