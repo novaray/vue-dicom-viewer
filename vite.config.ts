7a654878964cb04d4c2a92d4f457ced77a9aba2c
skkim <novaray@naver.com>
2024-01-13 23:43:45 +0900

    netlify 배포 테스트.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import cp from 'vite-plugin-cp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    cp({
+      targets: [
+        {
+          src: "node_modules/@cornerstonejs/dicom-image-loader/dist/dynamic-import/*",
+          dest: "dist",
+        },
+      ],
+    })
   ],
   resolve: {
     alias: {
